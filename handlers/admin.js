36e87490792861fc292dffe8cca2098438f77a28
Ammar Yusef <amar.yousef@gmail.com>
2021-01-03 23:42:00 +0200

    add some fix to routers/admins.js ...

--- handlers/admin.js
+++ handlers/admin.js
@@ -6,10 +6,12 @@ const adminSignUp = (req, res, next) => {
   model
     .addAdmin(req.body)
     .then((response) => {
-        res.status(201).send(response);
+      const { password, ...restDetails } = response;
+      res.status(201).send(restDetails);
     })
     .catch((err) => {
       console.log(err);
+      next(err);
     });
 };
 
---
+++ middleware/admin.js
@@ -0,0 +1,1 @@
+const checkPassedParams = (req, res, next) => {};

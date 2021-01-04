11bd573132dc29594a3c35ad5a2dff800bf2eaf3
Ammar Yusef <amar.yousef@gmail.com>
2021-01-04 13:43:23 +0200

    add packges and donation budget routes...

--- routers/admin.js
+++ routers/admin.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { adminSignUp } = require("../handlers/admin");
+const { adminSignUp, addPackges, addDonation } = require("../handlers/admin");
 const { checkUserIfExists } = require("../middleware/users");
 const { checkSignUpParams } = require("../middleware/admin");
 const router = express.Router();
 
 router.post("/sign-up", checkSignUpParams, checkUserIfExists, adminSignUp);
-
+router.post("/add-packages", addPackges);
+router.post("/add-donation", addDonation);
 module.exports = router;
